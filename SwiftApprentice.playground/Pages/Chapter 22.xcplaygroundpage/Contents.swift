ae5212863ee7b34437dd68e02ffce88c3d66f64d
raxavelo <raxavelo@gmail.com>
2021-11-30 14:17:41 +0100

    Added solution for challenge 2 in chapter 22

--- SwiftApprentice.playground/Pages/Chapter 22.xcplaygroundpage/Contents.swift
+++ SwiftApprentice.playground/Pages/Chapter 22.xcplaygroundpage/Contents.swift
@@ -26,4 +26,27 @@ do {
 } catch {
   print("Not a number!")
 }
+
+/*
+ Challenge 2: Safe division
+ 
+ Write a throwing function that divides type Int types.
+ */
+
+enum DivisionErrors: Error {
+  case divisionByZero
+}
+
+func divide(_ a: Int, by b: Int) throws -> Int {
+  guard b != 0 else {
+    throw DivisionErrors.divisionByZero
+  }
+  return a / b
+}
+
+do {
+  try divide(4, by: 0)
+} catch {
+  print("You can't divide by 0!")
+}
 //: [Next](@next)
